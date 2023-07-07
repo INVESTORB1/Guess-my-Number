f766d8ccb29d9de054504e5f1425645125ab0b4e
MIR <pbank0001@gmail.com>
2023-07-07 10:10:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 "use strict";
+const clearInput = function(){
+	document.querySelector('.guess').value= '';
+};
 
 let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -11,26 +14,33 @@ let highscore = document.querySelector(".highscore").textContent;
 document.querySelector(".check").addEventListener("click", function () {
 	// console.log(document.querySelector('.guess').value);
 	let guess = Number(document.querySelector(".guess").value);
-	console.log(guess);
 	// console.log(typeof guess);
+
 
 	//When there is no input from the user
 	if (!guess) {
 		document.querySelector(".message").textContent = "No Answer";
 	} else if (guess == secretNumber) {
+		clearInput();
 		document.querySelector(".message").textContent = "Correct Answer";
 
 		document.querySelector(".number").textContent = secretNumber;
 
 		document.querySelector("body").style.backgroundColor = "#60b347";
 		// document.querySelector('.score').textContent =score;
+		
 
 		document.querySelector(".number").style.width = "15rem";
 
 		document.querySelector(".highscore").textContent = score;
-
-		//When the input is less than the score
-	} else if (guess < secretNumber) {
+//Calling the clear input function that is created at the top of this code
+		
+// console.log
+		
+	}
+	//When the input is less than the score
+	else if (guess < secretNumber) {
+		  clearInput();
 		if (score > 1) {
 			document.querySelector(".message").textContent =
 				"Less than Correct Answer";
@@ -52,7 +62,12 @@ document.querySelector(".check").addEventListener("click", function () {
 			document.querySelector(".score").textContent = 0;
 		}
 	}
-});
+}
+	
+
+);
+
+
 
 //For the Again button
 document.querySelector(".again").addEventListener("click", function () {
@@ -63,6 +78,8 @@ document.querySelector(".again").addEventListener("click", function () {
 	document.querySelector(".number").style.width = "12rem";
 	document.querySelector(".number").textContent = "?";
 	document.querySelector("body").style.backgroundColor = "#222";
+	clearInput();
+	
 });
 
 //For the Reset button
@@ -75,6 +92,8 @@ document.querySelector(".Reset").addEventListener("click", function () {
 	document.querySelector(".highscore").textContent = 0;
 	document.querySelector(".score").textContent = score;
 	document.querySelector(".number").textContent = "?";
+	// document.querySelector(".guess").value= '';
+	clearInput();
 });
 
 
